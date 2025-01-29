1dd5185b5bca6269b5d5be6c6d92077dd11e3451
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-28 20:46:13 -0600

    Adding process

--- js/index.js
+++ js/index.js
@@ -4,13 +4,31 @@ function calcular() {
     let strC = document.getElementById("valorC").value
     
     if (strA === "") {
-        alert("Valor a en blanco")
+        alerta("Valor a en blanco")
     } else if(strB === "") {
-        alert("Valor b en blanco")
+        alerta("Valor b en blanco")
     } else if(strC === "") {
-        alert("Valor c en blanco")
+        alerta("Valor c en blanco")
     } else {
-        alert("Todo bien")
+        let a = parseInt(strA)
+        let b = parseInt(strB)
+        let c = parseInt(strC)
+
+        if (a !== 0) {
+            let discriminante = (b * b) - (4 * a * c)
+
+            if (discriminante >= 0) {
+                let x1 = (-b + Math.sqrt(discriminante)) / (2 * a)
+                let x2 = (-b - Math.sqrt(discriminante)) / (2 * a)
+
+                document.getElementById("valorX1").value = x1
+                document.getElementById("valorX2").value = x2
+            } else {
+                alerta("La raíz cuadrada no puede ser negativa")
+            }
+        } else {
+            alerta("Valor de a no puede ser igual a cero")
+        }
     }
 }
 
@@ -18,4 +36,13 @@ function limpiar() {
     document.getElementById("valorA").value = ""
     document.getElementById("valorB").value = ""
     document.getElementById("valorC").value = "" 
+    document.getElementById("valorX1").value = ""
+    document.getElementById("valorX2").value = ""
+}
+
+function alerta(mensaje) {
+    Swal.fire({
+      icon: "warning",
+      title: mensaje,
+    });
 }
